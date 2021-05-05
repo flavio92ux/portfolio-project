a730f1af6e5a2b4ff017aa0744fbd19740dc9620
flavio f <flavio.win@gmail.com>
2021-05-04 22:06:00 -0300

    about me part 1

---
+++ src/components/AboutMe.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import author from '../portfolio-files/me.jpg';
+
+const AboutMe = () => {
+  return (
+    <div className="container py-5">
+      <div className="row">
+        <div className="col-lg-6 col-xm-12"></div>
+          <img src={author} alt="author..." />
+      </div>
+      <div className="col-lg-6 col-xm-12"></div>
+      <h1>about me</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type 
+          specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
+          publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+    </div>
+  )
+}
+
+export default AboutMe
+
