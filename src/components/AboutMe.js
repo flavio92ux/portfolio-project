1f7aba55698a72ea65c0bb95507eee09663b3436
flavio f <flavio.win@gmail.com>
2021-05-05 10:01:46 -0300

    service complete

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,10 +13,13 @@ const AboutMe = () => {
       <div className="col-lg-6 col-xm-12"></div>
       <h1 className="about-heading">about me</h1>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type 
-          specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
+          has been the industry's standard dummy text ever since the 1500s, when an unknown 
+          printer took a galley of type and scrambled it to make a type 
+          specimen book. It has survived not only five centuries, but also the leap 
+          into electronic typesetting, remaining essentially unchanged. 
+          It was popularised in the 1960s with the release of Letraset sheets 
+          containing Lorem Ipsum passages, and more recently with desktop 
           publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </p>
       </div>
